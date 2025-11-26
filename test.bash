16eac2c7e58b3882c1d25a31d2d040eee6b0051c
Kazuki Nakagawa <s24c1089eq@s.chibakoudai.jp>
2025-11-26 12:56:15 +0900

    アスキー文字変換器

--- test.bash
+++ test.bash
@@ -1,4 +1,24 @@
 #!/bin/bash
 
+
+EXPECTED_OUTPUT_SNIPPET="ttttt eeeee  ssss"
+
 ACTUAL_OUTPUT=$(echo "TEST" | ./ascii)
+
+if [[ "$ACTUAL_OUTPUT" == *"$EXPECTED_OUTPUT_SNIPPET"* ]]; then
+    echo "OK: ascii command passed."
+else
+    echo "NG: ascii command failed."
+    exit 1
+fi
+
 if echo "!" | ./ascii 2> /dev/null; then
+
+    echo "NG: failed to exit with error."
+    exit 1
+else
+    echo "OK: failed as expected."
+fi
+
+echo "All tests passed."
+exit 0
